70ec7b5f0750b75dd10e4ebd53cc0685f9d7119c
Akbarjon <akbarisroilovdragon@gmail.com>
2022-06-11 15:15:10 -0400

    use localstorage in main script

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -7,5 +7,19 @@ navController.addEventListener("click", () => {
   navController.classList.toggle("to-right");
   main.classList.toggle("full");
   //
+  if (sideBar.classList.contains('hidden')) {
+    localStorage.setItem("sideBarHidden", "yes");
+  }
+  else {
+    localStorage.setItem("sideBarHidden", "no");
+  }
+  //
   // document.body.classList.toggle('dark-theme');
+  console.log(localStorage);
 });
+
+if (localStorage.getItem("sideBarHidden") == "yes") {
+  sideBar.classList.toggle("hidden");
+  navController.classList.toggle("to-right");
+  main.classList.toggle("full");
+}
